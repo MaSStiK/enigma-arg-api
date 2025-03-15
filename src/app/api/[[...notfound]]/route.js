d82b2c1b3e026897690448534aad16910030ffb2
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-03-15 01:43:52 +0100

    notfound

---
+++ src/app/api/[[...notfound]]/route.js
@@ -0,0 +1,19 @@
+const REPLY = [
+    {text: `Reply from 77.85.11.60: `, speed: "instant"},
+    {text: `Система не может найти указанный файл...`, newLine: false},
+    {text: "&nbsp;"}
+]
+
+export async function GET(req) {
+    return new Response(
+        JSON.stringify(REPLY),
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET",
+            },
+            status: 400,
+        }
+    );
+}
