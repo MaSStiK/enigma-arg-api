1041b020eb85464b8378c8a435f3b388e60e2f2c
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-07-22 19:10:30 +0200

    little secret

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Console from "@/components/Console/Console"
-
 export default function Home() {
     return (
-        <p>Тут ничего нету</p>
+        <p>Тут ничего нету <span style={{color: "#FFFFFF"}}>(Честно)</span></p>
     );
 }
